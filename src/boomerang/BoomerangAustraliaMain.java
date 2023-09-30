159a0d9f88e6cf6c16cac9df2cd637836d01c848
syko240 <andrepost@hotmail.se>
2023-09-30 18:51:03 +0200

    main

---
+++ src/boomerang/BoomerangAustraliaMain.java
@@ -0,0 +1,15 @@
+package boomerang;
+
+public class BoomerangAustraliaMain {
+
+    public BoomerangAustraliaMain(String[] argv) {
+    }
+
+    public static void main(String argv[]) {
+        try {
+            new BoomerangAustraliaMain(argv);
+        } catch(Exception e) {
+
+        }
+    }
+}
